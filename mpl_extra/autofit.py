300732eaff7e2f1a916bbaaf5fb193b8c8cac113
chenyulue <chenyulue@163.com>
2022-03-27 22:35:04 +0800

    autofit

--- mpl_extra/autofit.py
+++ mpl_extra/autofit.py
@@ -1,3 +1,6 @@
+import math
+import textwrap
+
 import matplotlib.patches as mpatches
 import matplotlib.text as mtext
 import matplotlib.transforms as mtrans
@@ -47,11 +50,8 @@ def text_with_autofit(
     width_in_pixels, height_in_pixels = dist2pixels(transform, width, height)
     
     render = txtobj.axes.get_figure().canvas.get_renderer()
-    rotation = txtobj.get_rotation()
     fontsize = txtobj.get_fontsize()
     
-    # Set the rotation of the text to be zero so that the bbox reflects the 
-    # width and height of the rendered text
     bbox = txtobj.get_window_extent(render)
     
     adjusted_fontsize = min(fontsize * width_in_pixels / bbox.width,
@@ -60,36 +60,119 @@ def text_with_autofit(
     txtobj.set_fontsize(adjusted_fontsize)
     
     if show_rect: 
-        # Get the position of the text in pixels.    
-        x0, y0 = txtobj.get_position()
-        ha, va = txtobj.get_horizontalalignment(), txtobj.get_verticalalignment()
+        # Get the position of the text bounding box in pixels.    
+        x0, y0, *_ = txtobj.get_window_extent(render).bounds
         
-        # Transform the text position into the position in the current coordinates.
-        x0, y0 = transform.inverted().transform(
-            txtobj.get_transform().transform((x0, y0))
-        )
+        # Transform the box position into the position in the current coordinates.
+        x0, y0 = transform.inverted().transform((x0, y0))
         
-        xa0 = {
-            'center': x0 - width / 2,
-            'left': x0,
-            'right': x0 - width,
-        }[ha]
-        ya0 = {
-            'center': y0 - height / 2,
-            'bottom': y0,
-            'top': y0 - height,
-            'baseline': y0,
-            'center_baseline': y0 - height / 2,
-        }[va]
-        
-        rect = mpatches.Rectangle((xa0, ya0), width, height, angle=rotation,
-                                  fill=False, ls='--', transform=transform)
+        rect = mpatches.Rectangle(
+            (x0, y0), 
+            width, 
+            height, 
+            fill=False, ls='--', transform=transform)
         txtobj.axes.add_patch(rect)
         
         return txtobj, rect
         
     return txtobj
         
+
+def text_with_autowrap(
+    txtobj,
+    width,
+    height,
+    *,
+    transform=None,
+    show_rect=False
+):
+ 
+    import textwrap
+       
+    if width < 0 or height < 0:
+        raise ValueError('`width` and `height` should be a number >= 0.')
+    
+    if txtobj.get_rotation():
+        raise ValueError('`wrap` option only supports the text object with a 0 rotation.')
+    
+    if transform is None:
+        transform = txtobj.get_transform()
+        
+    # Get the width and height of the box in pixels.
+    width_in_pixels, height_in_pixels = dist2pixels(transform, width, height)
+    
+    render = txtobj.axes.get_figure().canvas.get_renderer()
+    dpi = txtobj.axes.get_figure().get_dpi()
+    max_fontsize = pixels2points(
+        dpi,
+        height_in_pixels
+        )
+    
+    txtobj.set_fontsize(max_fontsize)
+    bbox = txtobj.get_window_extent(render)
+    
+    if bbox.width <= width_in_pixels:
+        return txtobj
+    
+    line_num = 2
+    aspect_ratio = bbox.width / len(txtobj.get_text()) / bbox.height  # This varies with the font!!
+    while True:
+        scaled_fontsize_in_pixels = calc_fontsize_in_pixels(
+            height_in_pixels, 
+            line_num, 
+            txtobj._linespacing
+            )
+        pixels_per_char = aspect_ratio * scaled_fontsize_in_pixels
+        wrap_length = max(1, width_in_pixels // pixels_per_char)
+        wrapped_text = textwrap.wrap(txtobj.get_text(), int(wrap_length))
+        
+        if len(wrapped_text) <= line_num:
+            break
+        
+        line_num += 1
+        
+    txtobj.set_text('\n'.join(wrapped_text))
+    txtobj.set_fontsize(pixels2points(
+        dpi,
+        scaled_fontsize_in_pixels
+    ))
+        
+    if show_rect: 
+        # Get the position of the text bounding box in pixels.    
+        x0, y0, *_ = txtobj.get_window_extent(render).bounds
+        
+        # Transform the box position into the position in the current coordinates.
+        x0, y0 = transform.inverted().transform((x0, y0))
+        
+        rect = mpatches.Rectangle(
+            (x0, y0), 
+            width, 
+            height, 
+            fill=False, ls='--', transform=transform)
+        txtobj.axes.add_patch(rect)
+        
+        return txtobj, rect
+    
+    return txtobj
+
+def calc_fontsize_in_pixels(height, n, linespacing):
+    """Calculate the fontsize according to the box height and wrapped lines.
+
+    Parameters
+    ----------
+    height : float
+        The height of the box
+    n : int
+        The number of wrapped lines
+    linespacing : float
+        The line spacing of the text.
+
+    Returns
+    -------
+    float
+        The fontsize
+    """    
+    return height / (n * linespacing - linespacing + 1)
         
         
 def dist2pixels(transform, dist, *dists):
@@ -143,3 +226,43 @@ def pixels2dist(transform, dist, *dists):
     list of float, corresponding to the arguments passed into the function.
     """
     return dist2pixels(transform.inverted(), dist, *dists)
+
+
+def pixels2points(dpi, pixels):
+    """Convert display units in pixels to points
+
+    Parameters
+    ----------
+    dpi : float
+        The figure dpi
+    pixels : float
+        The pixel numbers
+
+    Returns
+    -------
+    float
+        The points for fontsize, linewidth, etc.
+    """    
+    inch_per_point = 1 / 72
+    return pixels / dpi / inch_per_point
+
+# Refence https://stackoverflow.com/questions/4018860/text-box-with-line-wrapping-in-matplotlib
+def min_dist_inside(point, rotation, box):
+    """Get the space in a given direction from `point` to the boundaries of
+    `box`.
+
+    Parameters
+    ----------
+    point : tuple of float
+        The text start point
+    rotation : float
+        The text angle in degrees
+    box : an object with `x0`, `y0`, `x1` and `y1` attributes
+        The box region
+        
+    Returns
+    -------
+    float
+        The minum distance inside the box
+    """ 
+    pass   
