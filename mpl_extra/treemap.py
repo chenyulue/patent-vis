6ca45ca04b4739f7afdab884ad4f7b24462a0982
chenyulue <chenyulue@163.com>
2022-04-01 22:38:07 +0800

    refactor the treemap function

---
+++ mpl_extra/treemap.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 11:13:56 2022
+
+@author: Chenyu Lue
+"""
+
+def treemap(
+    axes,
+    data,
+    area,
+    *,
+    labels=None,
+    fill=None,
+    levels=None,
+    norm_x=100, 
+    norm_y=100,
+    top=False,    
+):
+    pass
